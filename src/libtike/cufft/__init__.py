23d3063945a47b44901b2dd25dd71eec97cf1229
Xiaodong Yu <xyu@mona1.xray.aps.anl.gov>
2020-05-15 02:13:06 -0500

    Remove old and unnecessary files

--- src/libtike/cufft/__init__.py
+++
@@ -1,9 +0,0 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
-from libtike.cufft.ptycho import *
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
